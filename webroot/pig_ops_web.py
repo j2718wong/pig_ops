9d708d74ba30f56336bf881739a9c581727b25ce
zhaoshan99 <zhaoshan99@gmail.com>
2025-02-17 13:13:43 +0800

    remove reload=True to prevent multiple loading of modules

--- webroot/pig_ops_web.py
+++ webroot/pig_ops_web.py
@@ -19,7 +19,7 @@ async def root():
 
 if __name__ == '__main__':
     
-    uvicorn.run("pig_ops_web:app", host='0.0.0.0', port=8080, reload=True)
+    uvicorn.run("pig_ops_web:app", host='0.0.0.0', port=8080)
         
 
 
