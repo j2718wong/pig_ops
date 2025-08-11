34ad237a9ef27ab5dfcdcffe8aa567aa54ea349a
zhaoshan99 <zhaoshan99@gmail.com>
2025-08-12 07:25:18 +0800

    more codes

--- webroot/route/r_account.py
+++ webroot/route/r_account.py
@@ -157,3 +157,73 @@ async def account_update(account_data: dm.DataAccount):
 
         
     return res_update
+    
+    
+@app.post("/account/request_add_user")
+async def account_request_add_user(account_data: dm.DataAccount):
+    name    = account_data.name
+    uhid    = account_data.uhid
+    
+    name    = name.strip() if name else None 
+    
+    if name is None or len(name) == 0:
+        return {
+            'result':{
+                'num':  ERROR_ACCOUNT_INVALID_NAME,
+                'code': 'ERROR_ACCOUNT_INVALID_NAME',
+                'desc': ''
+            }
+        }
+        
+    
+    res = hashids_user.decrypt(uhid)
+    if len(res) == 0:
+        return {
+            'result':{
+                'num':  ERROR_USER_INVALID_USER_HASHID,
+                'code': 'ERROR_USER_INVALID_USER_HASHID',
+                'desc': ''
+            }
+        }
+    
+    
+    user_id = res[0]
+    
+    
+    data = {
+        'name':             name,
+        'user_id':          user_id
+    }
+    
+    
+    res_register    =  model['account'].register(data)
+    
+    if res_register is None:
+        return {
+            'result':{
+                'num':  ERROR_DATABASE_ERROR,
+                'code': 'ERROR_DATABASE_ERROR',
+                'desc': ''
+            }
+        }
+    
+    account_id      = res_register['account']['id']
+    account_flag    = res_register['account']['flag']
+        
+    account_hashid  = hashids_account.encrypt(account_id)
+    
+    # remove plain id
+    del res_register['account']['id']
+    res_register['account']['h_id'] = account_hashid
+
+    result_num      = res_register['result']['num']
+    
+    if result_num == ACCOUNT_REGISTER_RES_NUM_SUCCESS:
+        data = {
+           'account_id':    account_id,
+           'hashid':        account_hashid
+        }
+        res_update = model['account'].update_hashid(data)
+        
+    return res_register
+    
