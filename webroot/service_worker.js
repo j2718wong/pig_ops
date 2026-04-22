7483bdb4c8cfcfcf275b6043de59507680e68e93
Jack Wong <j2718wong@gmail.com>
2026-04-22 20:50:21 +0800

    into to PWA

---
+++ webroot/service_worker.js
@@ -0,0 +1,28 @@
+const CACHE_NAME = 'superpig-v1';
+
+self.addEventListener('install', (event) => {
+    console.log('Service Worker installed');
+    self.skipWaiting();
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request).then((response) => {
+            return response || fetch(event.request);
+        })
+    );
+});
+
+self.addEventListener('activate', (event) => {
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.map((cacheName) => {
+                    if (cacheName !== CACHE_NAME) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+});
